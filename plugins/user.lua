cbc18491c45ffd33229c73d0cb67a80cb652d414
ashymee <ashymee@gmail.com>
2023-04-17 03:01:36 +0700

    update conf

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,28 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open Todo in Telescope" },
+    },
+  },
+  -- {
+  --   "akinsho/flutter-tools.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "stevearc/dressing.nvim", -- optional for vim.ui.select
+  --   },
+  --   opts = {},
+  --   event = "User AstroFile",
+  --   -- lazy = false,
+  -- },
+  { "dart-lang/dart-vim-plugin" },
+  { "thosakwe/vim-flutter" },
+  { "natebosch/vim-lsc" },
+  { "natebosch/vim-lsc-dart" },
 }
