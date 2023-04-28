579e171e5a7f3bf142f72cc86a103b8d23de5aad
ashymee <ashymee@gmail.com>
2023-04-28 19:58:09 +0700

    add markdown preview plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,21 +5,11 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- Colorscheme
   { import = "astrocommunity.pack.typescript" },
-  {
-    import = "astrocommunity.colorscheme.catppuccin",
-    -- opts = {
-    --   transparent_background = true,
-    -- },
-  },
-  { import = "astrocommunity.colorscheme.everforest" },
-  { import = "astrocommunity.colorscheme.gruvbox" },
-  { import = "astrocommunity.colorscheme.kanagawa" },
-  {
-    import = "astrocommunity.colorscheme.mini-base16",
-  },
-  { import = "astrocommunity.colorscheme.nightfox" },
-  { import = "astrocommunity.colorscheme.oxocarbon" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  -- Markdown preview
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 }
