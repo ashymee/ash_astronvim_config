a1e9bbf3bf39f6da4eecf2d0ccd24cf62ac2b79e
ashymee <ashymee@gmail.com>
2023-05-23 23:36:18 +0700

    remove unused lines

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,23 +8,23 @@ return {
       ensure_installed = {
         -- LSP/Mason                       TS [TreeSitter]
         -- -------------------------------------------------------------
-        "lua_ls",        -- lua
-        "tsserver",      -- javascript & typescript
-        "jsonls",        -- json & jsonc
-        "marksman",      -- markdown
-        "prismals",      -- prisma
-        "yamlls",        -- yaml
-        "emmet_ls",      -- html
-        "intelephense",  -- php
-        "pyright",       -- python
-        "sqlls",         -- sql
-        "svelte",        -- svelte
-        "tailwindcss",   -- css
-        "vuels",         -- vue
-        "csharp_ls",     -- c_sharp
-        "rust_analyzer", -- rust
-        "graphql",       -- graphql
-        "zls",           -- zig
+        -- "lua_ls",        -- lua
+        -- "tsserver",      -- javascript & typescript
+        -- "jsonls",        -- json & jsonc
+        -- "marksman",      -- markdown
+        -- "prismals",      -- prisma
+        -- "yamlls",        -- yaml
+        -- "emmet_ls",      -- html
+        -- "intelephense",  -- php
+        -- "pyright",       -- python
+        -- "sqlls",         -- sql
+        -- "svelte",        -- svelte
+        -- "tailwindcss",   -- css
+        -- "vuels",         -- vue
+        -- "csharp_ls",     -- c_sharp
+        -- "rust_analyzer", -- rust
+        -- "graphql",       -- graphql
+        -- "zls",           -- zig
         -- "prettierd",
         -- "dockerls",                        -- dockerfile
         -- "docker_compose_language_service", -- dockerfile
