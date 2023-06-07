f4cc4c628501595892e8d73bce7ac91355670021
ashymee <ashymee@gmail.com>
2023-06-08 05:51:39 +0700

    ensure installed mason null-ls listed

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,5 +1,54 @@
 return {
-    {"williamboman/mason-lspconfig.nvim", opts = {}},
-    {"jay-babu/mason-null-ls.nvim", opts = {}},
-    {"jay-babu/mason-nvim-dap.nvim", opts = {}}
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+          "astro",
+          "bashls",
+          "cssls",
+          "docker-compose-language-server",
+          "emmet_ls",
+          "gopls",
+          "html",
+          "jsonls",
+          "lua_ls",
+          "marksman",
+          "nginx-language-server",
+          "omnisharp", -- dotnet
+          "prismals",
+          "pyright",
+          "pylsp",
+          "rust_analyzer",
+          "sql_ls",
+          "svelte",
+          "tailwindcss",
+          "taplo", -- toml
+          "vuels",
+          "vimls",
+          "zls", -- zig
+        })
+    end,
+  },
+
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+          "black",
+          "blade-formatter",
+          "prettierd",
+          "sqlfmt",
+          "stylua",
+          "yamlfmt",
+          "eslint_d",
+        })
+    end,
+  },
+
+  -- debugger
+  { "jay-babu/mason-nvim-dap.nvim", opts = {} },
 }
