704c349b6887c9b95af4f23ac2fbfc75bd2fc3f8
Aswin Sahlan Himawan <ashymee@gmail.com>
2023-06-27 18:57:50 +0700

    update conf

--- init.lua
+++ init.lua
@@ -121,16 +121,16 @@ return {
     },
   },
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
     },
   },
   diagnostics = { virtual_text = true, underline = true },
@@ -178,6 +178,7 @@ return {
         require("typescript").setup {
           server = opts,
           root_dir = require("lspconfig.util").root_pattern ".git",
+          flags = { debounce_text_changes = 150 },
         }
       end,
       rust_analyzer = function(_, opts)
